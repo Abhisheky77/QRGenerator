f851cb2b94e9931bef9edb21f2e4c729bba0dd03
Abhisheky77 <123infinity890@gmail.com>
2026-01-15 20:05:55 +0530

    QRGenerator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+import QRGenerator from './components/QRGenerator'
+
+function App() {
+  
+  return (
+    <>
+      <QRGenerator/>  
+    </>
+  )
+}
+
+export default App
